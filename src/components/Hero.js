d68458a3264f821d9619f8bf285d740f336561b3
Harold Tai <harodtai321@gmail.com>
2022-03-20 22:52:52 +0800

    First commit

---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function HeroSection() {
+  return (
+    <h1>Hero Section</h1>
+  );
+}
+
+export default HeroSection;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Navbar Goes here</a>
+    </nav>
+  );
+}
+
+export default Navbar;
